adb29e862549d7f6828158bc52aefcf765c552e6
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-11-30 21:50:36 +0100

    added eventlisteners for carousel buttons

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const main = document.querySelector("#main");
+const cards = document.querySelectorAll(".usercard");
+let cardPosition = 0;
+const totalCards = cards.length;
+
+document
+  .getElementById("btn-next-user")
+  .addEventListener("click", moveToNextCard);
+document
+  .getElementById("btn-prev-user")
+  .addEventListener("click", moveToPrevCard);
+
+function moveToNextCard() {
+  if (cardPosition === totalCards - 1) {
+    cardPosition = 0;
+  } else {
+    cardPosition++;
+  }
+}
+
+function moveToPrevCard() {
+  console.log("moving to the prev card!");
+}
